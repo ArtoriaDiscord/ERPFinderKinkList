65b89f21ec4ef161c7ddcbf97e2dda0bb3e15555
ArtoriaDiscord <130687104+ArtoriaDiscord@users.noreply.github.com>
2023-06-23 14:52:10 +0300

    Added theme changer script and button

--- def-theme.js
+++ def-theme.js
@@ -1,19 +1,40 @@
 const systemDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-if (systemDarkMode) {
-    localStorage.setItem("theme", "dark")
-}
-else {
-    localStorage.setItem("theme", "light")
-}
+let btn = null
+document.addEventListener('DOMContentLoaded', function() {
+    btn = document.querySelector(".theme-btn img")
+    if (localStorage.getItem("theme") === null) {
+        if (systemDarkMode) {
+            localStorage.setItem("theme", "dark")
+        }
+        else {
+            localStorage.setItem("theme", "light")
+        }
+    }
+    applyTheme();
+    btn.addEventListener('click', changeTheme)
+});
 
 function applyTheme() {
-    const themeStylesheet = document.getElementById('themeStylesheet');
+    let themeStylesheet = document.getElementById('themeStylesheet');
     if (localStorage.getItem("theme") === "dark") {
+        btn.src = 'service_res/sun.png'
         themeStylesheet.href = 'dark-theme.css';
     } else {
+        btn.src = 'service_res/moon.png'
         themeStylesheet.href = 'light-theme.css';
     }
 }
 
-applyTheme();
+function changeTheme() {
+    let themeStylesheet = document.getElementById('themeStylesheet');
+    if (localStorage.getItem("theme") === "dark") {
+        btn.src = 'service_res/moon.png'
+        themeStylesheet.href = 'light-theme.css';
+        localStorage.setItem("theme", "light")
+    } else {
+        btn.src = 'service_res/sun.png'
+        themeStylesheet.href = 'dark-theme.css';
+        localStorage.setItem("theme", "dark")
+    }
+}
+
