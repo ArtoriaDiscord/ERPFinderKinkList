09230c97a3f788bb20ce56e7a173f7ecfa8365eb
ArtoriaDiscord <130687104+ArtoriaDiscord@users.noreply.github.com>
2023-06-24 16:15:23 +0300

    added mobile style, added welcome msg

--- modal-first-notification.js
+++ modal-first-notification.js
@@ -1,8 +1,11 @@
 let modal_status = localStorage.getItem("firstJoin")
 let modal_window = null
+let modal_btn = null
 
 document.addEventListener("DOMContentLoaded", function() {
     modal_window = document.querySelector(".modal-background")
+    modal_btn = document.querySelector(".ok-modal")
+    modal_btn.addEventListener("click", close_modal)
     if (modal_status === null) {
         localStorage.setItem("firstJoin", "true")
     }
@@ -10,3 +13,8 @@ document.addEventListener("DOMContentLoaded", function() {
         modal_window.style.display = "none";
     }
 })
+
+
+function close_modal()  {
+    modal_window.style.display = "none"
+}
