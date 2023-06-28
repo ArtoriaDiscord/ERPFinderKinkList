40dde156a48bb25a78b25fee8aa23d83e37ddb04
ArtoriaDiscord <130687104+ArtoriaDiscord@users.noreply.github.com>
2023-06-28 18:44:17 +0300

    Update desktop-kink-survey.js

--- desktop-kink-survey.js
+++ desktop-kink-survey.js
@@ -7,6 +7,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const like_btn = document.querySelector(".btn-forward")
     dislike_btn.addEventListener("click", dislike_kink)
     like_btn.addEventListener("click", like_kink)
+    card.addEventListener('touchstart', handleTouchStart);
+    card.addEventListener('touchmove', handleTouchMove);
+    card.addEventListener('touchend', handleTouchEnd);
 });
 
 function like_kink() {
@@ -47,4 +50,38 @@ function generate_new_card() {
     if (deck_counter > 4) {
         deck_counter = 1;
     }
+}
+
+function handleTouchStart(event) {
+    initialX = event.touches[0].clientX;
+}
+
+function handleTouchMove(event) {
+    if (initialX === null) {
+        return;
+    }
+
+    currentX = event.touches[0].clientX;
+}
+
+function handleTouchEnd() {
+    if (currentX !== null) {
+        const diffX = currentX - initialX;
+        const threshold = 100;
+
+        if (Math.abs(diffX) > threshold) {
+            if (diffX < 0) {
+                console.log('Swipe left');
+                dislike_kink()
+            } else {
+                console.log('Swipe right');
+                like_kink()
+            }
+        } else {
+            card.style.transform = 'translateX(0)';
+        }
+    }
+
+    initialX = null;
+    currentX = null;
 }
